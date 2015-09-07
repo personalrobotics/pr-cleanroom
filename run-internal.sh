448a4f233335ae455b6e6e70bd45cdfc8249cc9b
Michael Koval <mkoval@cs.cmu.edu>
2015-09-07 14:18:18 -0400

    Added support for file:// URIs.

--- run-internal.sh
+++ run-internal.sh
@@ -1,45 +1,35 @@
 #!/bin/bash -e
 
-if [ "$#" -ne 1 ]; then
-    echo 'error: Incorrect number of arguments.' 1>&2
-    echo 'usage: ./run-internal.sh <rosinstall-uri>' 1>&2
-    exit 1
-fi
-
-rosinstall_uri="$1"
-
-# Clean up any previous runs.
-# TODO: This is not sufficient because it does not remove installed packages.
-rm -rf /home/ubuntu
-mkdir -p /home/ubuntu
-cd /home/ubuntu
-
-# Create a Catkin workspace.
+echo "Creating Catkin workspace."
+wstool init src
 catkin init
 catkin config --extend "/opt/ros/${ROS_DISTRO}" --cmake-args -DCMAKE_BUILD_TYPE=Release
-
-# Checkout the .rosinstall file using wstool.
-# TODO: Take the URI to the rosinstall file as an argument.
-wstool init src
 cd src
-wget -O remote.rosinstall "${rosinstall_uri}"
-wstool merge -y remote.rosinstall
-# TODO: This is a hack until my changes to or_trajopt are merged.
-wstool set -y or_trajopt -v bugfix/dependencies
+
+echo "Merging .rosinstall files."
+for rosinstall_uri in "$@"; do
+    wstool merge -y "${rosinstall_uri}"
+done
+
+echo "Checking out repositories."
+git config --global credential.helper cache
 wstool update
 
 # Delete manifest.xml files because they confuse rosdep.
+echo "Deleting 'manifest.xml' files."
 find . -name manifest.xml -delete
 
-# Install dependencies using rosdep.
+# Install dependencies using rosdep. 
+echo "Installing dependencies."
 rosdep update
 apt-get update
 eatmydata rosdep install -y --ignore-src --from-paths .
 
-# Build the packages (and unit tests) using catkin.
+echo "Building."
 catkin build
+
+echo "Building tests."
 catkin build --catkin-make-args tests
 
-# Run the unit tests.
-# TODO: Also check the test results using catkin_test_results.
+echo "Running tests."
 catkin build --catkin-make-args run_tests
--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ if [ "${is_running}" != "true" ]; then
     echo "Building the apt-cache-ng Docker image."
     docker build -f Dockerfile_apt-cacher-ng -t apt-cacher-ng .
 
-    echo "Starting apt-cacher-ng Docker container."
+    echo "Starting the apt-cacher-ng Docker container."
     docker rm apt-cacher-ng
     docker run -d --name=apt-cacher-ng apt-cacher-ng
 else
@@ -15,8 +15,23 @@ fi
 echo "Building the client Docker image."
 docker build -f Dockerfile_client -t pr-cleanroom .
 
+echo "Staging .rosinstall files."
+staging_dir=$(mktemp -d)
+staged_uris=()
+for rosinstall_uri in "$@"; do
+    rosinstall_name=$(basename "${rosinstall_uri}")
+    staged_uris+=("file:///rosinstalls/${rosinstall_name}")
+
+    echo "Staging '${rosinstall_uri}' => '{staging_dir}/${rosinstall_name}'"
+    curl -o "${staging_dir}/${rosinstall_name}" "${rosinstall_uri}"
+done
+
 echo "Starting the client Docker container."
-echo docker run -it --link=apt-cacher-ng:apt-cacher-ng pr-cleanroom ./run-internal.sh $*
-docker run -it --link=apt-cacher-ng:apt-cacher-ng pr-cleanroom ./run-internal.sh $*
+docker run -it \
+    --link=apt-cacher-ng:apt-cacher-ng \
+    --volume=${staging_dir}:/rosinstalls \
+    pr-cleanroom \
+    ./run-internal.sh ${staged_uris[@]}
 
 # TODO: Copy the test results out of the Docker container.
+# TODO: Check the test results using catkin_test_results.
