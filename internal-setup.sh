f8e10f355da57b61cff12889b0014fb2b29b9c5b
Michael Koval <mkoval@cs.cmu.edu>
2015-10-24 21:08:34 -0400

    Tweaks for Docker.

--- internal-setup.sh
+++ internal-setup.sh
@@ -1,12 +1,15 @@
 #!/bin/bash -ex
+APTGET='apt-get -qqy'
+CURL='curl -sS'
+SUDO='sudo -n'
 
 # Add the ROS apt repository.
-sudo -n apt-key adv --keyserver ha.pool.sks-keyservers.net --recv-keys 421C365BD9FF1F717815A3895523BAEEB01FA116
-echo 'deb http://packages.ros.org/ros/ubuntu trusty main' > /etc/apt/sources.list.d/ros-latest.list
+${SUDO} apt-key adv --keyserver ha.pool.sks-keyservers.net --recv-keys 421C365BD9FF1F717815A3895523BAEEB01FA116
+${SUDO} sh -c 'echo "deb http://packages.ros.org/ros/ubuntu trusty main" > /etc/apt/sources.list.d/ros-latest.list'
 
 # Install ROS build tools.
-sudo -n apt-get -qq update
-sudo -n apt-get -qqy install --no-install-recommends \
+${SUDO} ${APTGET} update
+${SUDO} ${APTGET} install --no-install-recommends \
   ca-certificates \
   curl \
   eatmydata \
@@ -17,13 +20,12 @@ sudo -n apt-get -qqy install --no-install-recommends \
   python-rosinstall \
   python-wstool \
   ros-indigo-ros-core \
-  subversion \
-sudo -n rm -rf /var/lib/apt/lists/*
+  subversion
 
 # Add the PR apt repository.
-curl https://www.personalrobotics.ri.cmu.edu/files/personalrobotics.gpg | sudo -n apt-key add -
-echo 'deb http://packages.personalrobotics.ri.cmu.edu/public trusty main' > /etc/apt/sources.list.d/personalrobotics.list
+${CURL} 'https://www.personalrobotics.ri.cmu.edu/files/personalrobotics.gpg' | ${SUDO} apt-key add -
+${SUDO} sh -c 'echo "deb http://packages.personalrobotics.ri.cmu.edu/public trusty main" > /etc/apt/sources.list.d/personalrobotics.list'
 
 # Setup rosdep with our custom keys.
-sudo -n rosdep init
-curl -o /etc/ros/rosdep/sources.list.d/10-pr.list https://raw.githubusercontent.com/personalrobotics/pr-rosinstalls/master/rosdep/10-pr.list
+${SUDO} rosdep init
+${SUDO} ${CURL} -o '/etc/ros/rosdep/sources.list.d/10-pr.list' 'https://raw.githubusercontent.com/personalrobotics/pr-rosinstalls/master/rosdep/10-pr.list'
