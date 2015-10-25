9711667f1f3f78d032d21cbb6a8a977c6f09427e
Michael Koval <mkoval@cs.cmu.edu>
2015-10-24 21:25:27 -0400

    Removed some comments.

--- internal-build.sh
+++ internal-build.sh
@@ -1,32 +1,20 @@
-#!/bin/bash -e
+#!/bin/bash -ex
+build_path="/build"
+output_path="/test_results"
 
-echo "Creating Catkin workspace."
 catkin init
 catkin config --extend "/opt/ros/${ROS_DISTRO}" --cmake-args -DCMAKE_BUILD_TYPE=Release
 cd src
 
-# Delete manifest.xml files because they confuse rosdep.
-echo "Deleting 'manifest.xml' files."
 find . -name manifest.xml -delete
 
-# Install dependencies using rosdep. 
-echo "Installing dependencies."
-rosdep update
 apt-get update
-eatmydata rosdep install -y --ignore-src --from-paths .
+rosdep update
+rosdep install -y --ignore-src --from-paths .
 
-echo "Building."
 catkin build
-
-echo "Building tests."
 catkin build --catkin-make-args tests
-
-echo "Running tests."
 catkin build --catkin-make-args run_tests
-
-echo "Copying test results."
-build_path="/build"
-output_path="/test_results"
 
 for package_name in $(ls "${build_path}"); do
     if [ -d "${build_path}/${package_name}/test_results/${package_name}" ]; then
@@ -35,9 +23,7 @@ for package_name in $(ls "${build_path}"); do
     fi
 done
 
-echo "Displaying test results:"
 cd ..
 ./view-all-results.sh "${output_path}"
 
-# Generate a non-zero return code if any of the tests failed.
 catkin_test_results /test_results
