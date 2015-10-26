a753bbdbb80cded20d20a3efb6bbe7f57d7bdc2c
Michael Koval <mkoval@cs.cmu.edu>
2015-10-26 14:35:40 -0400

    Updated run-distro to match new script.

--- run-distro.sh
+++ run-distro.sh
@@ -4,16 +4,17 @@ distribution_uri="$1"
 repository="$2"
 package_names="$(./internal-get-packages.py ${distribution_uri} ${repository})"
 
-staging_dir=$(mktemp -d)
+test_dir="$(mktemp -d)"
+staging_dir="$(mktemp -d)"
 cp "${distribution_uri}" "${staging_dir}/distribution.yml"
 
 docker build -f Dockerfile_client -t pr-cleanroom .
-
-test_dir=$(mktemp -d)
 docker run -it \
-    --volume=${staging_dir}:/distribution\
-    --volume=${test_dir}:/test_results \
+    --volume="${staging_dir}:/distribution"\
+    --volume="${test_dir}:/test_results" \
     pr-cleanroom \
-    bash -c "./internal-distro.py --workspace=src --repository ${repository} distribution/distribution.yml && ./internal-build.sh ${package_names}"
+    bash -c "./internal-distro.py --workspace=src --repository ${repository} distribution/distribution.yml \
+          && ./internal-build.sh ${package_names} \
+          && ./internal-test.sh ${package_names}"
 
 echo "Test results are in: ${test_dir}"
