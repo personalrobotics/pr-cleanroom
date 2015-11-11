b38ac20b1be415d84391179bfd587349271f0497
Michael Koval <mkoval@cs.cmu.edu>
2015-11-11 17:33:01 -0500

    Added rospack installation.

--- internal-setup.sh
+++ internal-setup.sh
@@ -27,6 +27,7 @@ ${SUDO} ${APTGET} install --no-install-recommends \
   python-wstool \
   python-vcstools \
   ros-indigo-ros-core \
+  ros-indigo-rospack \
   subversion
 
 # Add the PR apt repository.
