925663a1388bc700c79aab6404a42bb65b4606a3
Jeongseok Lee <jslee02@users.noreply.github.com>
2018-04-12 17:09:40 -0700

    Use package-full.xml when available

--- internal-distro.py
+++ internal-distro.py
@@ -76,7 +76,6 @@ def main():
     parser.add_argument('distribution_file', type=str)
     parser.add_argument('--package', type=str, action='append', dest='target_packages', default=[])
     parser.add_argument('--repository', type=str, action='append', dest='target_repositories', default=[])
-    parser.add_argument('--manifest_name', type=str, default='package.xml')
     args = parser.parse_args()
 
     if not os.path.exists(args.workspace):
@@ -167,7 +166,7 @@ def main():
             # Search for packages in the repository.
             repository_package_map = dict()
             rospkg.list_by_path(
-                manifest_name=args.manifest_name,
+                manifest_name='package.xml',
                 path=repository.location,
                 cache=repository_package_map)
 
@@ -203,7 +202,9 @@ def main():
             installed_packages.update(repository_package_map.iterkeys())
 
         # Crawl dependencies.
-        package_xml_path = os.path.join(package.location, args.manifest_name)
+        package_xml_path = os.path.join(package.location, 'package-full.xml')
+        if not os.path.isfile(package_xml_path):
+            package_xml_path = os.path.join(package.location, 'package.xml')
         package_manifest = parse_package(package_xml_path)
 
         all_depends = set()
